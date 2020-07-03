d809bcd7930dee186332adaf4e113365b378a155
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-03 17:15:51 -0400

    CSSmore

--- src/Components/FavItem.js
+++ src/Components/FavItem.js
@@ -6,10 +6,12 @@ export class FavItem extends Component {
     render() {
      
         return (
-            <div className="card-holer">
+            <div className="card-holder" id='pink'>
             <div className='ui card'>
                 <h4 className='header'>{this.props.item.title} </h4>
+                <div>
                 <Link to={`/items/${this.props.item.id}`}><img className='ui large image' src={this.props.item.picture} alt={this.props.item.title} /> </Link>
+                </div>
             </div>
             </div>
         )
