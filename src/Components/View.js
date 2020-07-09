b6ca99cc6ce75eb0bce6221caa630a78f40fe15e
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-07-08 20:05:23 -0400

    fixedMonthsAddition

--- src/Components/View.js
+++ src/Components/View.js
@@ -3,17 +3,17 @@ import '../Styles/Sale.css'
 
 export class View extends Component {
     render() {
-        let month = this.props.months[Math.floor(Math.random() * this.props.months.length)] 
         let clicks = this.props.clicks.map(click => click.item_id)
         let filterClick = clicks.sort((a,b) => a = b ? 1 : -1 )
         let clicksAmout = filterClick.filter((v, i, a) => a.indexOf(v) === i)
         let highestClick = clicksAmout[0]
         let amount = this.props.clicks.filter(click => click.item_id === this.props.item.id )
         let color = amount.length === highestClick ? '#232345' : '#EF5809' 
+        let month = this.props.months[amount] 
   
         return (
             <div className='stats-container'>
-                <div>{this.props.item.title} recieved {amount.length} clicks this {month}</div>
+                <div>{this.props.item.title} recieved {amount.length} clicks this month</div>
                 {/* <div><strong>{amount.length === highestClick ? 'Your highest clicked item this month' : null }</strong></div> */}
                 <div id="barContainer">
                     <div className="donationBar" style={{width: `${this.props.item.price}%`, backgroundColor: '#232345' }}></div>
